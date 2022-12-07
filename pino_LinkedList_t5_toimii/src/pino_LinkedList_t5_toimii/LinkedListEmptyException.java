a65acf6ebe23720618c4517f77604f78a28590db
Tomila <55088024+Tomila@users.noreply.github.com>
2022-12-07 11:10:41 +0200

    Add files via upload

---
+++ pino_LinkedList_t5_toimii/src/pino_LinkedList_t5_toimii/LinkedListEmptyException.java
@@ -0,0 +1,16 @@
+package pino_LinkedList_t5_toimii;
+
+public class LinkedListEmptyException extends RuntimeException {
+	
+	private static final Long serialVersionUID = 1L;
+	
+	public LinkedListEmptyException() {
+		super();
+	}	
+	
+	public LinkedListEmptyException(String message) {
+		super(message);
+
+}
+	
+}
